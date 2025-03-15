835e08edbeaf9653b4184557d7708de4759264fd
becherzribi <zribibacher@tbs.u-tunis.tn>
2025-03-15 22:49:24 +0100

    Update configuration for static export and deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  output: "standalone", // This is important for Docker
+  output: 'export', 
   images: {
     domains: ["github.com"],
   },
